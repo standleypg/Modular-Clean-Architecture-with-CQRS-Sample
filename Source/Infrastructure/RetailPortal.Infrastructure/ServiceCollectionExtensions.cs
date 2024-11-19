2329cc61d5bcb67ec7f4cf08dc340dcb14414938
Standley Gury <standleypg@gmail.com>
2024-11-19 19:37:00 +0800

    Finalized project structure and added dependencies diagram

---
+++ Source/Infrastructure/RetailPortal.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RetailPortal.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
