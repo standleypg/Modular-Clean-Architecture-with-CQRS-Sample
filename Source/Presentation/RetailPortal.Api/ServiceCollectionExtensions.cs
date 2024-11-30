bbe4c10fcf86ea4db1b0553ece25579775779552
standleypg <standleypg@gmail.com>
2024-12-01 01:36:05 +0800

    Added API versioning

--- Source/Presentation/RetailPortal.Api/ServiceCollectionExtensions.cs
+++ Source/Presentation/RetailPortal.Api/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using Asp.Versioning;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace RetailPortal.Api;
 
@@ -6,13 +8,47 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApi(this IServiceCollection services)
     {
-        services.AddAutoMapper();
+        services
+            .AddOpenApi()
+            .AddAutoMapper();
         return services;
     }
 
     private static IServiceCollection AddAutoMapper(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        return services;
+    }
+
+    private static IServiceCollection AddOpenApi(this IServiceCollection services)
+    {
+        services.AddOpenApi("v0",options =>
+        {
+            options.AddDocumentTransformer((document, context, cancellationToken) =>
+            {
+                document.Info = new OpenApiInfo
+                {
+                    Title = "Retail Portal API - v0.0", Version = "0.0"
+                };
+
+                return Task.CompletedTask;
+            });
+        });
+
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(0, 0);
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("X-Api-Version"));
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'V";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
         return services;
     }
 }
