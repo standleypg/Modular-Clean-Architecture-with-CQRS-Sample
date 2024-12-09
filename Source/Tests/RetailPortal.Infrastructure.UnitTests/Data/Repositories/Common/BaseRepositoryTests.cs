242e51744438d57dc1e2b4385c3fa73daafd7c65
Standley Gury <standleypg@gmail.com>
2024-12-09 21:05:57 +0800

    Updated tests folder structure

---
+++ Source/Tests/RetailPortal.Infrastructure.UnitTests/Data/Repositories/Common/BaseRepositoryTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using RetailPortal.Infrastructure.Data.Context;
+
+namespace RetailPortal.Infrastructure.UnitTests.Data.Repositories.Common;
+
+public class BaseRepositoryTests: IDisposable
+{
+    protected ApplicationDbContext Context { get; }
+
+    protected BaseRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: $"RetailPortal-{Guid.NewGuid().ToString()}")
+            .Options;
+        
+        Context = new ApplicationDbContext(options);
+        
+        Context.Database.EnsureCreated();
+        Context.Database.EnsureDeleted();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            Context.Dispose();
+        }
+    }
+
+    ~BaseRepositoryTests()
+    {
+        Dispose(false);
+    }
+}
