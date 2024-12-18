80901f70e86bb8e4576b58a9cb4badbf686a3b20
standleypg <standleypg@gmail.com>
2024-12-18 22:18:57 +0800

    Added infra Auth unit tests

---
+++ Source/Tests/RetailPortal.Infrastructure.UnitTests/Auth/JwtTokenGeneratorTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using RetailPortal.Domain.Interfaces.Infrastructure.Services;
+using RetailPortal.Infrastructure.Auth;
+using RetailPortal.Infrastructure.UnitTests.Data.Repositories.Common;
+using RetailPortal.Shared.Constants;
+
+namespace RetailPortal.Infrastructure.UnitTests.Auth;
+
+public class JwtTokenGeneratorTests
+{
+    private readonly JwtTokenGenerator _sut;
+
+    public JwtTokenGeneratorTests()
+    {
+        Mock<IDateTimeProvider> dateTimeProviderMock = new();
+        Mock<IOptions<Appsettings.JwtSettings>> jwtOptionsMock = new();
+        dateTimeProviderMock.Setup(d => d.UtcNow).Returns(DateTime.Now);
+        jwtOptionsMock.Setup(j => j.Value).Returns(new Appsettings.JwtSettings
+        {
+            Secret = "long secret for HMACSHA512 algorithm to be used for signing the JWT token",
+            ExpirationMinutes = 30
+        });
+        this._sut = new JwtTokenGenerator(dateTimeProviderMock.Object, jwtOptionsMock.Object);
+    }
+
+    [Fact]
+    public void GenerateToken_ShouldReturnToken()
+    {
+        // Arrange
+        var user = RepositoryUtils.CreateUser(1);
+
+        // Act
+        var token = this._sut.GenerateToken(user);
+
+        // Assert
+        Assert.NotNull(token);
+    }
+}
---
+++ Source/Tests/RetailPortal.Infrastructure.UnitTests/Auth/PasswordHasherTests.cs
@@ -0,0 +1,50 @@
+﻿using RetailPortal.Infrastructure.Auth;
+
+namespace RetailPortal.Infrastructure.UnitTests.Auth;
+
+public class PasswordHasherTests
+{
+    private readonly PasswordHasher _sut = new();
+
+    [Fact]
+    public void CreatePasswordHash_ShouldCreatePasswordHash()
+    {
+        // Arrange
+        var password = "password";
+
+        // Act
+        this._sut.CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+        // Assert
+        Assert.NotNull(passwordHash);
+        Assert.NotNull(passwordSalt);
+    }
+
+    [Fact]
+    public void VerifyPasswordHash_ShouldReturnTrue_WhenPasswordIsCorrect()
+    {
+        // Arrange
+        var password = "password";
+        this._sut.CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+        // Act
+        var result = this._sut.VerifyPasswordHash(password, passwordHash, passwordSalt);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void VerifyPasswordHash_ShouldReturnFalse_WhenPasswordIsIncorrect()
+    {
+        // Arrange
+        var password = "password";
+        this._sut.CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+        // Act
+        var result = this._sut.VerifyPasswordHash("incorrectPassword", passwordHash, passwordSalt);
+
+        // Assert
+        Assert.False(result);
+    }
+}
